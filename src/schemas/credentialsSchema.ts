e5192aa1c51d54e8599a13456cce0c23f33a2d91
pedro imbrech <elprogramadorpedro@gmail.com>
2022-08-01 16:57:59 -0300

    rev

--- src/schemas/credentialsSchema.ts
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-import { CreateCredentialData } from "../repositories/credentialsRepository.js";
-
-export const createCredentialSchema = joi.object<CreateCredentialData>({
-  title: joi.string().required(),
-  url: joi.string().uri().required(),
-  username: joi.string().required(),
-  password: joi.string().required(),
-});
